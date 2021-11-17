f174fef9df6debc4fcfeb8fda0ff969a3f14fa36
Nicholas Kapralov <4dvlup@gmail.com>
2021-11-17 15:22:33 +0100

    Added stub for popup tool

--- assets/scripts/ToolPopUp.gd
+++ assets/scripts/ToolPopUp.gd
@@ -1,10 +1,14 @@
 extends Node
 
 
-var input_data = [1, 3, 5, 7, 9]
-var output_data = [1, 3, 5, 7, 9]
-
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
     pass # Replace with function body.
+
+
+func _on_Button_mouse_entered():
+    $Sprite.visible = true
+
+
+func _on_Button_mouse_exited():
+    $Sprite.visible = false
